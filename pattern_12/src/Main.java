489981f00636f0f2a0a5d55f80ee2cd35932bdc2
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2023-06-28 20:55:01 +0530

    add question 12

--- pattern_12/src/Main.java
+++ pattern_12/src/Main.java
@@ -3,13 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print("   ");
             }
-            for (int k = 0; k <= i; k++) {
-                System.out.print("  *   ");
+            for (int k = 0; k < 2*i-1; k++) {
+                if (k%2!=0) {
+                    System.out.print(" ! ");
+                } else {
+                    System.out.print(" * ");
+                }
             }
             System.out.println();
         }
